e31213e18d55d36af61e9f27466b744cb83edfc4
Navnath3864 <nagutte0@gmail.com>
2025-04-28 00:12:27 +0530

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Navnath3864/',  // very important!
+  base: '/MyPortfolioRepo/',  // very important!
 })
 
